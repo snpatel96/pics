77ee3b119aed8f2f291b5c15d8074507d5ab404a
snehal patel <snpatel.dev@gmail.com>
2022-11-15 20:54:40 -0500

    #2 controlled elements & form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
+  state = { term: '' };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.term);
+  };
 
   render() {
     return (
       <div>
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
